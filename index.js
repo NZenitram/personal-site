e020049480a45d6ff96e10fe14d064dbb0cefe07
250ok-prod <nicholasmartinez@gmail.com>
2018-01-22 06:58:46 -0700

    updated with pull from s3

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 function inputCommand(command, term){
   var input = command.toLowerCase()
   if(input == 'help') {
-    term.echo(new String("Reveal: Reveals links \n\nGitHub: Opens up my GitHub profile \n\nLinkedIn: Opens my LinkedIn profile \n\nTuring: Opens my Turing School profile \n\nResume: Opens a page with my resume \n\nBlog: Opens my blog "));
+    term.echo(new String("GitHub: Opens up my GitHub profile \n\nLinkedIn: Opens my LinkedIn profile \n\nTuring: Opens my Turing School profile"));
   } else if(input == 'github') {
     window.open("https://www.github.com/nzenitram");
   } else if(input == 'linkedin') {
     window.open("https://www.linkedin.com/in/nzenitram");
-  } else if(input == 'resume') {
-    window.open("http://www.nzenitram.com/blog/index.php/resume/");
-  } else if(input == 'blog') {
-    window.open("http://www.nzenitram.com/blog/")
-  } else if(input == 'reveal') {
-    $('.info-link').click()
   } else if(input == 'turing') {
     window.open("https://www.turing.io/alumni/nicholas-martinez")
   } else {
@@ -36,16 +30,33 @@ jQuery(function($, terminal) {
   {
     greetings: 'Type Help For a List of Commands:',
     name: 'js_demo',
-    height: 200,
+    height: 300,
     prompt: 'nm> '
     });
 });
 
-function unhide(){
-  $('#linkicons-header').removeAttr('style');
+function appendHtml() {
+  $(".blog").click(function() {
+    $.ajax({
+      'type' : 'GET',
+      'url' : this.name,
+      'success' : function(data){
+        $("#description").html(data)
+        $("body").scrollTop(0)
+        $('.button-collapse').sideNav('hide')
+      }
+    })
+  })
 }
 
 $( document ).ready(function() {
+  appendHtml()
+
+  $('.button-collapse').sideNav();
+
+  $(window).on('scroll', function() {
+    $('.button-collapse').sideNav('hide')
+  })
 
   $('#shadow').mouseover(function(){
     $(this).addClass('hover fa-lg');
@@ -55,22 +66,9 @@ $( document ).ready(function() {
     $(this).removeClass('hover fa-lg');
   });
 
-  setTimeout(function(){ unhide(); }, 3000);
-
   $(".button-collapse").sideNav();
 
-//   $(".blog").click(function() {
-//     $("#description").remove();
-//     $.ajax({
-//       'url' : 'https://s3-us-west-1.amazonaws.com/nzenitram-medium-posts/2016-11-27_My-Introduction-to-AWS-c23c83c2c1c4.html',
-//       'type' : 'GET',
-//       'headers' : {
-//           'Access-Control-Allow-Origin': '*'
-//         },
-//       'success' : function(data){
-//         $('#main').append(data)
-//       }
-//     })
-//     $('#m')
-//   })
+  $("#small-img").click(function() {
+    $("#description").html("<div id='header' class='center'><div id='name-header'><h2 id='name'>Nicholas Martinez</h1></div><div id='avatar'><img alt='Nicholas Martinez' class='figure z-depth-5' href='/' src='https://s3-us-west-1.amazonaws.com/nzenitramwp/Nick+Martinez-min.jpg'></div><div id='main'><p>Currently working remotely for 250OK, an Indianapolis based email data analytics company. I have been working primarily in Python and Selenium building a back-end application that integrates four separate platforms and operating systems. In general, I am working with UI automation across Windows, MacOS, iOS and Android operating systems.<br><br>Independently, I have been working on my AWS Certified Solutions Architect certification through aCloud.guru's labs and lessons. I have also spent time studying and learning React JS through Frontend Master's as well as Chef's Infrastructure as Code through learn.chef.io.</p>")
+  })
 });
